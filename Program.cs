4ef97af0ebbcb81b09fa841f6e9a02bb51e3b4f8
uhrinowitz <62422000+uhrinowitz@users.noreply.github.com>
2021-04-14 18:50:16 +0200

    teljes feladat kész
    teljes feladat kész

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace Balkezesek
 
             // 2. feladat
             List<Balkezesek> elsők = new List<Balkezesek>();
-            Console.WriteLine("2. feladat: 1980-ban lépett pályára:");
+            Console.WriteLine("\n2. feladat: 1980-ban lépett pályára:");
             foreach (Balkezesek balkezes in balkezesek)
             {
                 if (balkezes.Első.Contains("1980"))
@@ -39,15 +39,107 @@ namespace Balkezesek
             }
 
             // 3. feladat
+            Console.WriteLine($"\n3. feladat: Add meg egy játékos nevét:");
             string bekértNév = Console.ReadLine();
+            bool nincs = true;
             foreach (Balkezesek balkezes in balkezesek)
             {
                 if (balkezes.Név == bekértNév)
                 {
-                    Console.WriteLine($"3. feladat: {balkezes.Név} magassága: {balkezes.Magasság*2.54} cm.");
+                    Console.WriteLine($"{balkezes.Név} magassága: {balkezes.Magasság*2.54} cm.");
+                    nincs = false;
+                } 
+
+            }
+
+            if (nincs)
+            {
+                Console.WriteLine($"3. feladat: Hibás adat!"); 
+            }
+
+            // 4. feladat
+            Console.WriteLine("\n4. feladat: Adj meg évszámok 1900-1999 között:");
+            bool jó = false;
+            int évszám = 0;
+            do
+            {
+                évszám = int.Parse(Console.ReadLine());
+                if (évszám >= 1900 && évszám <= 1999)
+                {
+                    jó = true;
+                }
+                else
+                {
+                    Console.WriteLine("Adj meg évszámok 1900-1999 között:");
+                }
+            } while (!jó);
+
+            Console.WriteLine($"{évszám}-ban pályára lépettek adatai:");
+            foreach (Balkezesek balkezes in balkezesek)
+            {
+                if (balkezes.Első.Contains(évszám.ToString()))
+                {
+                    Console.Write($"Név: {balkezes.Név}, első: {balkezes.Első}, utolsó: {balkezes.Utolsó}, magasság: {balkezes.Magasság}, súly: {balkezes.Súly}\n");
                 }
             }
 
+            // 5. feladat
+            List<string> legkorábbiÉv = new List<string>();
+
+            foreach (Balkezesek balkezes in balkezesek)
+            {
+                legkorábbiÉv.Add(balkezes.Első);
+            }
+
+            legkorábbiÉv.Sort();
+            Console.WriteLine($"\n5. feladat: Legkorábbi év, amikor pályára léptek: {legkorábbiÉv[0].Split('-')[0]}");
+
+            // 6. feladat
+            List<int> kétezerElőttiek = new List<int>();
+            foreach (Balkezesek balkezes in balkezesek)
+            {
+                kétezerElőttiek.Add(int.Parse(balkezes.Utolsó.Split('-')[0]));
+            }
+            Console.WriteLine($"\n6. feladat: Mindenki 2000 előtt lépet pályára? -> {((kétezerElőttiek.Max() > 2000) ? "nem" : "igen")}");
+
+            // 7. feladat
+            Console.WriteLine($"\n7. feladat: John nevűek:\n");
+
+            bekértNév = "John";
+            int johnDb = 0;
+            foreach (Balkezesek balkezes in balkezesek)
+            {
+                if (balkezes.Név.Split(' ')[0] == bekértNév)
+                {
+                    johnDb++;
+                    Console.WriteLine($"\t{balkezes.Név}");
+                }
+            }
+            Console.WriteLine($"Összes John száma: {johnDb}");
+
+            // 8. feladat
+            Dictionary<string, int> keresztnevek = new Dictionary<string, int>();
+
+            foreach (Balkezesek balkezes in balkezesek)
+            {
+                string név = balkezes.Név.Split(' ')[0];
+                if (keresztnevek.ContainsKey(név))
+                {
+                    keresztnevek[név]++;
+                }
+                else
+                {
+                    keresztnevek.Add(név, 1);
+                }
+
+            }
+            Console.WriteLine("\n8. feladat");
+            string[] nevek = { "Joe", "John", "Jim", "Jack" };
+            int length = nevek.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{nevek[i]}-ból van: {keresztnevek[nevek[i]]}");
+            }
 
             Console.ReadLine();
         }

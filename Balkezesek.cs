7c78d0a37de170e8bea5c6f80e9ce9ff8dcb931d
uhrinowitz <62422000+uhrinowitz@users.noreply.github.com>
2021-04-14 17:53:20 +0200

    Initial commit

---
+++ Balkezesek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Balkezesek
+    {
+        public string Név { get; set; }
+        public string Első { get; set; }
+        public string Utolsó { get; set; }
+        public int Súly { get; set; }
+        public int Magasság { get; set; }
+
+        public Balkezesek(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Név = adatok[0];
+            Első = adatok[1];
+            Utolsó = adatok[2];
+            Súly = int.Parse(adatok[3]);
+            Magasság = int.Parse(adatok[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Név: Uhrin Ákos
+
+            string path = "balkezesek.csv";
+            List<Balkezesek> balkezesek = new List<Balkezesek>();
+            string[] sorok = File.ReadAllLines(path);
+
+            foreach (string sor in sorok.Skip(1))
+            {
+                balkezesek.Add(new Balkezesek(sor));
+            }
+
+            // 1. feladat
+            int versenyzőkSzáma = balkezesek.Count;
+            Console.WriteLine($"{versenyzőkSzáma} versenyzőről van adatunk.");
+
+            Console.ReadLine();
+        }
+    }
+}
